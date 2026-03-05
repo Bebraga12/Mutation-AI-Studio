b50dadda22ffec8f880847d46b530d16da9d04a2
Bernardo Braga <bebraga12@gmail.com>
2026-03-04 21:49:41 -0300

    Initial commit

---
+++ src/main/java/com/mutation/mutation_ai_studio/MutationAiStudioApplication.java
@@ -0,0 +1,13 @@
+package com.mutation.mutation_ai_studio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutationAiStudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutationAiStudioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mutation/mutation_ai_studio/MutationAiStudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mutation.mutation_ai_studio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutationAiStudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
